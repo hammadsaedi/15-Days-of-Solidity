b4e7679125e2e756d6150b9f9ddd3889718740a3
Hammad Saaedi <faoqzuhair@gmail.com>
2022-09-20 00:31:25 +0500

    GuessTheMagicWord

--- Day_14_ABI/41_Keccak256.sol
+++ Day_14_ABI/41_Keccak256.sol
@@ -90,3 +90,13 @@ contract Algo {
     // - Rearrange so, no dynamic data types are near to each other
     // - use encode instead of encodePacked
 }
+
+contract GuessTheMagicWord {
+    bytes32 public answer =
+        0x60298f78cc0b47170ba79c10aa3851d7648bd96f2f8e46a19dbc777c36fb0c00;
+
+    // Magic word is "Solidity"
+    function guess(string memory _word) public view returns (bool) {
+        return keccak256(abi.encodePacked(_word)) == answer;
+    }
+}
